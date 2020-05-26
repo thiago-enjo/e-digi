c373775cecce395f59818954f369fd14ee66eff5
Thiago Enjo <thiago.yuji@hotmail.com>
2020-05-26 16:23:41 -0300

    Adicionando Cadastro de Categoria

---
+++ src/br/com/alura/modelo/BancoCategorias.java
@@ -0,0 +1,22 @@
+package br.com.alura.modelo;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BancoCategorias {
+
+	private Set<Categoria> categorias = new HashSet<>();
+
+	public Set<Categoria> getCategorias() {
+		return Collections.unmodifiableSet(categorias);
+	}
+	
+	public void addCategoria(Categoria categoria) {
+		if (categorias.contains(categoria)) {
+			throw new RuntimeException("Categoria já cadastrada");
+		}
+		categorias.add(categoria);
+		System.out.println("Categoria " + categoria.getNome() + " cadastrada com sucesso!");
+	}
+}
---
+++ src/br/com/alura/modelo/Categoria.java
@@ -0,0 +1,41 @@
+package br.com.alura.modelo;
+
+import java.time.LocalDate;
+
+public class Categoria {
+
+	private String nome;
+	private LocalDate dataCadastro;
+	
+	public Categoria(String nome) {
+		if (nome == null || nome.isEmpty()) {
+			throw new IllegalArgumentException("Nome não pode estar vazio");
+		}
+		this.nome = nome;
+		this.dataCadastro = LocalDate.now();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	@Override
+	public String toString() {
+		return "\nNome: " + this.nome + "\nData de cadastro: " + this.dataCadastro;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Categoria outraCategoria = (Categoria) obj;
+		return this.nome.equals(outraCategoria.nome);
+	}
+	
+}
