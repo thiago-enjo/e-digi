577ee6e5b4d23453c7e3161c3e58de5ff73a1f60
Thiago Enjo <thiago.yuji@hotmail.com>
2020-06-12 15:58:08 -0300

    Adicionando método de busca por título
    Aqui eu fiquei na dúvida sobre o que fazer, criar uma nova classe ou implementar o método em 'BancoLivros'? Eu acho faz sentido ter esse método aqui mesmo já que essa classe está fazendo o papel do DAO, mas sinto que tem coisas que poderiam melhorar...

--- src/br/com/alura/modelo/BancoLivros.java
+++ src/br/com/alura/modelo/BancoLivros.java
@@ -19,4 +19,22 @@ public class BancoLivros {
 		livros.add(livro);
 		System.out.println("Livro " + livro.getTitulo() + " cadastrado com sucesso!");
 	}
+
+	public Set<Livro> buscaPorTitulo(String titulo) {
+		Set<Livro> resultado = new HashSet<>();
+		if (titulo == null) {
+			throw new IllegalArgumentException("Título não pode estar vazio");
+		} else if (titulo.length() < 2) {
+			throw new IllegalArgumentException("Número de caracteres insuficiente para realizar a busca");
+		}
+		for (Livro livro : livros) {
+			if (livro.getTitulo().toUpperCase().contains(titulo.toUpperCase())) {
+				resultado.add(livro);
+			}
+		}
+		if (resultado.isEmpty()) {
+			throw new RuntimeException("Título não encontrado");
+		}
+		return Collections.unmodifiableSet(resultado);
+	}
 }
