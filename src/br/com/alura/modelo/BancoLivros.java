9271dee51e4c3a307420049b11349f12c2d7ca21
Thiago Enjo <thiago.yuji@hotmail.com>
2020-06-18 18:15:02 -0300

    Alterando método de buscaPorTitulo
    Eu encontrei nos cursos de Java 8 da Alura uma forma de deixar o código sem aquele for e o Set criado na mão. Eu queria ter mandado uma NoSuchElementException caso nenhum título fosse encontrado, mas pelo menos até agora não consegui pensar em uma forma de fazer isso sem instanciar um Set antes :confused:

--- src/br/com/alura/modelo/BancoLivros.java
+++ src/br/com/alura/modelo/BancoLivros.java
@@ -3,6 +3,7 @@ package br.com.alura.modelo;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BancoLivros {
 
@@ -21,20 +22,13 @@ public class BancoLivros {
 	}
 
 	public Set<Livro> buscaPorTitulo(String titulo) {
-		Set<Livro> resultado = new HashSet<>();
 		if (titulo == null) {
 			throw new IllegalArgumentException("Título não pode estar vazio");
 		} else if (titulo.length() < 2) {
 			throw new IllegalArgumentException("Número de caracteres insuficiente para realizar a busca");
 		}
-		for (Livro livro : livros) {
-			if (livro.getTitulo().toUpperCase().contains(titulo.toUpperCase())) {
-				resultado.add(livro);
-			}
-		}
-		if (resultado.isEmpty()) {
-			throw new RuntimeException("Título não encontrado");
-		}
-		return Collections.unmodifiableSet(resultado);
+		return livros.stream().filter(livro -> livro.getTitulo()
+				.toUpperCase().contains(titulo.toUpperCase()))
+				.collect(Collectors.toSet());
 	}
 }
