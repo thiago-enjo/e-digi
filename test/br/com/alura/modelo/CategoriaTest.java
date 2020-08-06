f00ae912f9906719edf57409d57df783d5aaaac7
Thiago Enjo <thiago.yuji@hotmail.com>
2020-08-06 13:20:32 -0300

    Adicionando novo teste

--- test/br/com/alura/modelo/CategoriaTest.java
+++ test/br/com/alura/modelo/CategoriaTest.java
@@ -1,10 +1,17 @@
 package br.com.alura.modelo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 class CategoriaTest {
+	
+	@Test
+	public void deveCadastrarCategoriaComNome() {
+		
+		assertEquals("Programação", new Categoria("Programação").getNome());
+	}
 
 	@Test
 	public void lancaExcecaoCasoNomeDaCategoriaEstejaVazio() {
@@ -17,5 +24,4 @@ class CategoriaTest {
 
 		assertThrows(IllegalArgumentException.class, () -> new Categoria(null));
 	}
-
 }
